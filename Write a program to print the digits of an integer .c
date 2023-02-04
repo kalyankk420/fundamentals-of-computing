4d1d4175e003547bc97505ecc040b06af096301c
kalyankk420 <124229545+kalyankk420@users.noreply.github.com>
2023-02-04 14:07:24 +0530

    Update Write a program to print the digits of an integer .c

--- Write a program to print the digits of an integer .c
+++ Write a program to print the digits of an integer .c
@@ -1,43 +1,16 @@
 #include <stdio.h>
-#define MAX 100
-  
-// Function to print the digit of
-// number N
-void printDigit(int N)
-{
-    // To store the digit
-    // of the number N
-    int arr[MAX];
-    int i = 0;
-    int j, r;
-  
-    // Till N becomes 0
-    while (N != 0) {
-  
-        // Extract the last digit of N
-        r = N % 10;
-  
-        // Put the digit in arr[]
-        arr[i] = r;
-        i++;
-  
-        // Update N to N/10 to extract
-        // next last digit
-        N = N / 10;
-    }
-  
-    // Print the digit of N by traversing
-    // arr[] reverse
-    for (j = i - 1; j > -1; j--) {
-        printf("%d ", arr[j]);
-    }
-}
-  
-// Driver Code
+
 int main()
 {
-    int N = 3452897;
-  
-    printDigit(N);
-    return 0;
+int num, lastDigit;
+printf("Enter a number: ");
+scanf("%d", &num);
+
+while (num > 0)
+{
+    lastDigit = num % 10;
+    printf("%d ", lastDigit);
+    num = num / 10;
 }
+
+return 0;  
