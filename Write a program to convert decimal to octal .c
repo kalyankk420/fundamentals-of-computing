95005546aee4c382b4b8bb97fb3c8faf0d37e672
kalyankk420 <124229545+kalyankk420@users.noreply.github.com>
2023-02-04 14:03:56 +0530

    Update Write a program to convert decimal to octal .c

--- Write a program to convert decimal to octal .c
+++ Write a program to convert decimal to octal .c
@@ -1,27 +1,16 @@
 #include <stdio.h>
-#include <conio.h>
- 
-long decimalToOctal(long n);
+
 int main() {
-    long decimal;
-    printf("Enter a decimal number\n");
-    scanf("%ld", &decimal);
-    printf("Octal number of %ld is %ld", decimal, decimalToOctal(decimal));
-     
- getch();
-    return 0;
-}
- 
-/* Function to convert a decinal number to octal number */
-long decimalToOctal(long n) {
-    int remainder; 
- long octal = 0, i = 1;
-  
-    while(n != 0) {
-        remainder = n%8;
-        n = n/8;
-        octal = octal + (remainder*i);
-        i = i*10;
-    }
-    return octal;
+  int decimal, octal = 0, i = 1;
+  printf("Enter a decimal number: ");
+  scanf("%d", &decimal);
+
+  while (decimal != 0) {
+    octal = octal + (decimal % 8) * i;
+    decimal = decimal / 8;
+    i = i * 10;
+  }
+
+  printf("Octal representation of the decimal number: %d", octal);
+  return 0;
 }
