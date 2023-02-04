01d6da62323a25840c3b55d7bc4ba4db513eabee
kalyankk420 <124229545+kalyankk420@users.noreply.github.com>
2023-02-04 13:16:02 +0530

    Update Shell Program to swap two numbers .c

--- Shell Program to swap two numbers .c
+++ Shell Program to swap two numbers .c
@@ -1,11 +1,16 @@
-# Static input of the
-# number
-first=5
-second=10
-   
-temp=$first
-first=$second
-second=$temp
-  
-echo "After swapping, numbers are:"
-echo "first = $first, second = $second"
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d%d", &a, &b);
+    swap(&a, &b);
+    printf("After swapping: %d %d\n", a, b);
+    return 0;
+}
