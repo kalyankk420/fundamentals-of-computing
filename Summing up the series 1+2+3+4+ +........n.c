92198bf606bddbba18f2288fd29d24ffc0b448f1
kalyankk420 <124229545+kalyankk420@users.noreply.github.com>
2023-02-04 13:40:22 +0530

    Update Summing up the series 1+2+3+4+ +........n.c

--- Summing up the series 1+2+3+4+ +........n.c
+++ Summing up the series 1+2+3+4+ +........n.c
@@ -1,18 +1,14 @@
 #include<stdio.h>
-int main()
-{
-	int n, sum=0;
-	
-	printf("enter n value :");
-	scanf("%d",&n);
-	
-	for(int i=1; i<=n; i++)
-	{
-		printf("%d+",i);
-		sum +=i;	
-   }
-   
-   printf("\b=%d",sum);
-   
-   return 0;
+
+int main() {
+    int n, sum = 0;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++) {
+        sum = sum + i;
+    }
+
+    printf("The sum of the series 1+2+3+...+%d is %d\n", n, sum);
+    return 0;
 }
