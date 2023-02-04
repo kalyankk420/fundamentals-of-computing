724229102c7889525dd6ac13a44225fecc19fc5e
kalyankk420 <124229545+kalyankk420@users.noreply.github.com>
2023-02-04 14:22:38 +0530

    Update finding whether the given integer is odd or even .c

--- finding whether the given integer is odd or even .c
+++ finding whether the given integer is odd or even .c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-int main() {
-    int num;
-    printf("Enter an integer: ");
-    scanf("%d", &num);
 
-    // true if num is perfectly divisible by 2
-    if(num % 2 == 0)
-        printf("%d is even.", num);
-    else
-        printf("%d is odd.", num);
-    
-    return 0;
-}
+int main()
+{
+int number;
+printf("Enter an integer: ");
+scanf("%d", &number);
+if (number % 2 == 0)
+    printf("%d is an even number.\n", number);
+else
+    printf("%d is an odd number.\n", number);
+return 0;    
