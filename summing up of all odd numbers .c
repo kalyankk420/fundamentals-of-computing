bd12c82897345f09babce6aee168318a0d5f00dd
kalyankk420 <124229545+kalyankk420@users.noreply.github.com>
2023-02-04 14:47:58 +0530

    Update summing up of all odd numbers .c

--- summing up of all odd numbers .c
+++ summing up of all odd numbers .c
@@ -1,20 +1,12 @@
 #include <stdio.h>
 
-int main()
-{
-    int i, n, sum=0;
-
-    /* Input range to find sum of odd numbers */
-    printf("Enter upper limit: ");
-    scanf("%d", &n);
-
-    /* Find the sum of all odd number */
-    for(i=1; i<=n; i+=2)
-    {
-        sum += i;
-    }
-
-    printf("Sum of odd numbers = %d", sum);
-
-    return 0;
+int main() {
+int sum = 0, i;
+for (i = 1; i <= 100; i++) {
+if (i % 2 != 0) {
+sum += i;
+}
+}
+printf("Sum of all odd numbers from 1 to 100 is: %d", sum);
+return 0;
 }
