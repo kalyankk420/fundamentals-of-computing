fe708bbc8913c34e8adff1b264bc1873cd3878e7
kalyankk420 <124229545+kalyankk420@users.noreply.github.com>
2023-02-04 14:37:26 +0530

    Update product series [factorial of the given number] finding given number is armstrong or not .c

--- product series [factorial of the given number] finding given number is armstrong or not .c
+++ product series [factorial of the given number] finding given number is armstrong or not .c
@@ -1,16 +1,37 @@
 #include <stdio.h>
-int main()
+#include <math.h>
+
+int factorial(int n) 
 {
-long int a,n=1;
-printf("\nEnter the number :");
-scanf("%ld",&a);
-start:
-n=n*a;
-a--;
-if(a>0)
-{
-  goto start;
+    int result = 1;
+    while (n >= 1) 
+    {
+        result = result * n;
+        n--;
+    }
+    return result;
 }
-printf("\nThe Total value is :%ld",n);
-return 0;
+
+int main() 
+{
+    int num, temp, rem, digits, sum = 0;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    temp = num;
+    digits = (int) log10(num) + 1;
+
+    while (temp != 0) 
+    {
+        rem = temp % 10;
+        sum = sum + factorial(rem);
+        temp /= 10;
+    }
+
+    if (sum == num)
+        printf("%d is an Armstrong number.", num);
+    else
+        printf("%d is not an Armstrong number.", num);
+
+    return 0;
 }
