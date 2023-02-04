9828f30e5b694c8c3aa9c071d6921d446a208809
kalyankk420 <124229545+kalyankk420@users.noreply.github.com>
2023-02-04 14:32:17 +0530

    Update printing digits of an integer number .c

--- printing digits of an integer number .c
+++ printing digits of an integer number .c
@@ -1,14 +1,27 @@
 #include <stdio.h>
-int main() {   
-    int number;
-   
-    printf("Enter an integer: ");  
-    
-    // reads and stores input
-    scanf("%d", &number);
 
-    // displays output
-    printf("You entered: %d", number);
-    
+int main()
+{
+    int num, digits = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    // Finding the number of digits
+    int temp = num;
+    while (temp != 0)
+    {
+        temp /= 10;
+        digits++;
+    }
+
+    // Printing each digit
+    temp = num;
+    for (int i = 1; i <= digits; i++)
+    {
+        int digit = temp % 10;
+        printf("%d ", digit);
+        temp /= 10;
+    }
+
     return 0;
 }
