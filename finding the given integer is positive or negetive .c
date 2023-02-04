e4f613839af2db08624743c9c15de8d4be678c3d
kalyankk420 <124229545+kalyankk420@users.noreply.github.com>
2023-02-04 14:18:48 +0530

    Update finding the given integer is positive or negetive .c

--- finding the given integer is positive or negetive .c
+++ finding the given integer is positive or negetive .c
@@ -1,17 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main()
-{
+int main() {
     int num;
- 
-    scanf("%d",&num);
- 
-    if(num == 0)
-        printf("Neither positive nor negative");
-    else if(num < 0)
-        printf("Negative");
-    else
-        printf("Positive");
-     
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    if (num > 0) {
+        printf("%d is positive\n", num);
+    } else if (num < 0) {
+        printf("%d is negative\n", num);
+    } else {
+        printf("%d is zero\n", num);
+    }
+
     return 0;
 }
