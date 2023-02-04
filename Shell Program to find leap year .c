02ae4806069a0f5eae2d72a0954ff339125cd012
kalyankk420 <124229545+kalyankk420@users.noreply.github.com>
2023-02-04 11:50:38 +0530

    Update Shell Program to find leap year .c

--- Shell Program to find leap year .c
+++ Shell Program to find leap year .c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 int main() {
-   int year;
-   year = 2016;
+  int year;
+  printf("Enter a year: ");
+  scanf("%d", &year);
 
-   if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
-      printf("%d is a leap year", year);
-   else
-      printf("%d is not a leap year", year);
+  if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)))
+    printf("%d is a leap year.\n", year);
+  else
+    printf("%d is not a leap year.\n", year);
 
-   return 0;
+  return 0;
 }
