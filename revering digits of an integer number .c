8ffd3e469c63348b9d49d3cc2a4473ba5c6e7ba2
kalyankk420 <124229545+kalyankk420@users.noreply.github.com>
2023-02-04 14:40:06 +0530

    Update revering digits of an integer number .c

--- revering digits of an integer number .c
+++ revering digits of an integer number .c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 
-int main() {
-
-  int n, reverse = 0, remainder;
-
-  printf("Enter an integer: ");
-  scanf("%d", &n);
-
-  while (n != 0) {
-    remainder = n % 10;
-    reverse = reverse * 10 + remainder;
-    n /= 10;
-  }
-
-  printf("Reversed number = %d", reverse);
-
-  return 0;
+int main()
+{
+int num, reversed = 0;
+printf("Enter an integer: ");
+scanf("%d", &num);
+while(num != 0)
+{
+reversed = reversed * 10 + num % 10;
+num /= 10;
+}
+printf("Reversed number: %d\n", reversed);
+return 0;
 }
