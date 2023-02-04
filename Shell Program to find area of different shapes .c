3721b60ab35bc90cf86d2312072e9ffa7a4af357
kalyankk420 <124229545+kalyankk420@users.noreply.github.com>
2023-02-04 11:48:17 +0530

    Update Shell Program to find area of different shapes .c

--- Shell Program to find area of different shapes .c
+++ Shell Program to find area of different shapes .c
@@ -1,72 +1,31 @@
-#include<stdio.h>
-#include<math.h>
-main(){
-   int choice;
-   printf("Enter
-1 to find area of Triangle
-2 for finding area of Square
-3 for finding area of Circle
-4 for finding area of Rectangle
-5 for Parallelogram
-");
-   scanf("%d",&choice);
-   switch(choice) {
-      case 1: {
-         int a,b,c;
-         float s,area;
-         printf("Enter sides of triangle
-");
-         scanf("%d%d %d",&a,&b,&c);
-         s=(float)(a+b+c)/2;
-         area=(float)(sqrt(s*(s-a)*(s-b)*(s-c)));
-         printf("Area of Triangle is %f
-",area);
-         break;
-      }
-      case 2: {
-         float side,area;
-         printf("Enter Sides of Square
-");
-         scanf("%f",&side);
-         area=(float)side*side;
-         printf("Area of Square is %f
-",area);
-         break;
-      }
-      case 3: {
-         float radius,area;
-         printf("Enter Radius of Circle
-");
-         scanf("%f",&radius);
-         area=(float)3.14159*radius*radius;
-         printf("Area of Circle %f
-",area);
-         break;
-      }
-      case 4: {
-         float len,breadth,area;
-         printf("Enter Length and Breadth of Rectangle
-");
-         scanf("%f %f",&len,&breadth);
-         area=(float)len*breadth;
-         printf("Area of Rectangle is %f
-",area);
-         break;
-      }
-      case 5: {
-         float base,height,area;
-         printf("Enter base and height of Parallelogram
-");
-         scanf("%f %f",&base,&height);
-         area=(float)base*height;
-         printf("Enter area of Parallelogram is %f
-",area);
-         break;
-      }
-      default: {
-         printf("Invalid Choice
-");
-         break;
-      }
-   }
+#include <stdio.h>
+
+void square_area()
+{
+    int side;
+    printf("Enter the side of the square: ");
+    scanf("%d", &side);
+    int area = side * side;
+    printf("The area of the square is: %d\n", area);
 }
+
+void rectangle_area()
+{
+    int length, width;
+    printf("Enter the length of the rectangle: ");
+    scanf("%d", &length);
+    printf("Enter the width of the rectangle: ");
+    scanf("%d", &width);
+    int area = length * width;
+    printf("The area of the rectangle is: %d\n", area);
+}
+
+void triangle_area()
+{
+    int base, height;
+    printf("Enter the base of the triangle: ");
+    scanf("%d", &base);
+    printf("Enter the height of the triangle: ");
+    scanf("%d", &height);
+    int area = 0.5 * base * height;
+    printf("The area of the triangle is: …
