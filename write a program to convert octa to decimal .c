19be11a5449c1b7d84da174bf35e5200db5cfd16
kalyankk420 <124229545+kalyankk420@users.noreply.github.com>
2023-02-04 14:57:08 +0530

    Update write a program to convert octa to decimal .c

--- write a program to convert octa to decimal .c
+++ write a program to convert octa to decimal .c
@@ -1,31 +1,25 @@
-#include <stdio.h>
-#include <math.h>
-int OctalToDecimal(int num)
+#include<stdio.h>
+#include<math.h>
+
+int octToDecimal(int octal)
 {
-    int x = 0;
-    int ans = 0;
-
-    while (num > 0)
-    {
-
-        // obtaining the right-most digit of num
-        int y = num % 10;
-
-        // Since the right-most digits were previously scanned in the previous step, divide num by 10 to remove them.
-        num /= 10;
-
-        ans += y * pow(8, x);
-        ++x;
-    }
-
-    return ans;
+int decimal = 0;
+int i = 0;
+while (octal != 0)
+{
+decimal += (octal % 10) * pow(8, i);
+++i;
+octal /= 10;
 }
+return decimal;
+}
+
 int main()
 {
-    int num;
-    printf("Enter Octal Number: ");
-    scanf("%d", &num);
-    printf("\n");
-    printf("Decimal Number is : %d", OctalToDecimal(num));
-    return 0;
+int octal;
+printf("Enter an octal number: ");
+scanf("%d", &octal);
+int decimal = octToDecimal(octal);
+printf("Equivalent decimal value: %d", decimal);
+return 0;
 }
