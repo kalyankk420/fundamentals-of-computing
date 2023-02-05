f5e2d2072aa53e24dfa46a916551f6f820a62306
kalyankk420 <124229545+kalyankk420@users.noreply.github.com>
2023-02-05 16:53:10 +0530

    Create Shell Program factorial using recursion .c

---
+++ Shell Program factorial using recursion .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int factorial(int n) {
+if (n == 0)
+return 1;
+return n * factorial(n-1);
+}
+
+int main() {
+int n;
+printf("Enter a positive integer: ");
+scanf("%d", &n);
+printf("Factorial of %d = %d\n", n, factorial(n));
+return 0;
+}
