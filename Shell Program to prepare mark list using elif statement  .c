5b62f418b340b54c6194672fbb1d0dfb65226d3a
kalyankk420 <124229545+kalyankk420@users.noreply.github.com>
2023-02-05 16:34:13 +0530

    Create Shell Program to prepare mark list using elif statement  .c

---
+++ Shell Program to prepare mark list using elif statement  .c
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Enter marks (0-100):"
+read marks
+
+if [ $marks -ge 90 ]
+then
+  grade="A"
+elif [ $marks -ge 80 ]
+then
+  grade="B"
+elif [ $marks -ge 70 ]
+then
+  grade="C"
+elif [ $marks -ge 60 ]
+then
+  grade="D"
+else
+  grade="F"
+fi
+
+echo "Marks: $marks"
+echo "Grade: $grade"
